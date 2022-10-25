86f8605e3370ae03eb6b0fdc08e09044a504871b
sonjibony <sonjibony@gmail.com>
2022-10-25 15:21:15 +0600

    API created for courses

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+//using cors
+app.use(cors());
+
+//accessing course api
+const courses = require('./data/courses.json');
 
 app.get('/', (req,res) => {
     res.send('Edu Dive API running')
+})
+
+//sending courses data
+app.get('/courses',(req, res) => {
+res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse)
 })
 
 app.listen(port, () => {
